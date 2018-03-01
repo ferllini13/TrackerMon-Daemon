371fe88458589e0d1b7e7a0f78f815c84e7d16cb
Luis Saborio Hernandez <luisohz@gmail.com>
2018-03-01 14:56:14 -0600

    Se modificaron tipos de datos

--- include/File.h
+++ include/File.h
@@ -2,7 +2,7 @@
 #define _FILE_H
 
 void writeLog(int pType, double * pCurrentInfo, double * pThreshold, char * pSystemError, char * pLogFile);
-void readConfigFile(float * pCPU, float * pMEM, float * pSYNN, char * plogFilePath);
+void readConfigFile(double * pCPU, double * pMEM, double * pSYNN, char * pLogFilePath);
 
 
 #endif
